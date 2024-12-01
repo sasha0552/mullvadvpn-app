f05d5f703e311d9e56ce17b60e471d91a59b5bc4
sasha0552 <admin@sasha0552.org>
2024-12-01 23:37:03 +0000

    CI

--- ci/check-rust.sh
+++ ci/check-rust.sh
@@ -6,7 +6,7 @@ export RUSTFLAGS="--deny warnings"
 
 # Build Rust crates
 source env.sh
-time cargo build --locked --verbose
+time cargo build --locked --release --verbose
 
 # Test Rust crates
-time cargo test --locked --verbose
+time cargo test --locked --release --verbose
